fb50d4df373d558d94e57e53fc9e0f4dc154b7b1
dlpierce <dlpierce@indiana.edu>
2016-05-04 16:38:29 -0400

    Add file_size_ltsi solr field plus a megabyte sized version.

--- lib/hydradam/file_set_behavior/has_ffprobe.rb
+++ lib/hydradam/file_set_behavior/has_ffprobe.rb
@@ -10,7 +10,14 @@ module HydraDAM
         include DependsOn
         depends_on(ActiveFedora::Base)
 
-        property :file_format, predicate: RDF::Vocab::EBUCore.hasFileFormat
+        property :file_format, predicate: RDF::Vocab::EBUCore.hasFileFormat do |index|
+          index.as :symbol
+        end
+
+        # property :fileSize, predicate: RDF::Vocab::EBUCore.fileSize, multiple: false do |index|
+        #   # index.as Solrizer::Descriptor.new(:long, :stored, :searchable)
+        # end
+
  
         # Ensure module dependencies
         include Hydra::Works::FileSetBehavior
@@ -24,6 +31,7 @@ module HydraDAM
          noko.remove_namespaces!
          self.filename = noko.xpath('//ffprobe/format/@filename').text
          self.file_format += [noko.xpath('//ffprobe/format/@format_long_name').text]
+         self.file_size += [noko.xpath('//ffprobe/format/@size').text.to_i]
        end
      end
   end
